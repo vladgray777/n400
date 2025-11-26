ae8b364e117fc483f7b925ca60bacc776027d31b
Vladimir Krushinsky <VladimirK@kcd.local>
2025-11-26 15:41:45 -0500

    test

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,63 +1,39 @@
 // src/App.tsx
 
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+export interface QuestionItem {
+  question: string;
+  answers: string[];
+  numOfAnswers: number;
+  maxNumOfCorrectAnswers: number;
+}
+
+export interface QuestionsResponse {
+  [key: string]: QuestionItem;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
-  const response = await fetch("/api/questions");
-  
+  const [questions, setQuestions] = useState<QuestionsResponse>({});
+
+  const fetchData = async () => {
+    const response = await fetch("/api/questions");
+    const data: QuestionsResponse = await response.json();
+    console.log("Fetched:", data);
+    setQuestions(data);
+  };
+
+  // Run fetchData once when the component mounts
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://hono.dev/" target="_blank">
-          <img src={honoLogo} className="logo cloudflare" alt="Hono logo" />
-        </a>
-        <a href="https://workers.cloudflare.com/" target="_blank">
-          <img
-            src={cloudflareLogo}
-            className="logo cloudflare"
-            alt="Cloudflare logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React + Hono + Cloudflare</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)} aria-label="increment">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="card">
-        <button
-          onClick={() => {
-            fetch("/api/")
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name));
-          }}
-          aria-label="get name"
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the logos to learn more Vlad</p>
-    </>
+    <div>
+      <h1>Questions</h1>
+      <pre>{JSON.stringify(questions, null, 2)}</pre>
+    </div>
   );
 }
 
